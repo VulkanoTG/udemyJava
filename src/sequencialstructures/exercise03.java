40cc4aa8d0e296853263262adf68d1e19979e1b1
VulkanoTG <pedrohh114@gmail.com>
2022-07-07 07:36:08 -0300

    Resolved ex '1,2,3,4'

--- src/sequencialstructures/exercise03.java
+++ src/sequencialstructures/exercise03.java
@@ -1,5 +1,24 @@
 package sequencialstructures;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class exercise03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, value;
+        System.out.println("Digite o Primeiro número!");
+        a = sc.nextInt();
+        System.out.println("Digite o segundo número!");
+        b = sc.nextInt();
+        System.out.println("Digite o terceiro número!");
+        c = sc.nextInt();
+        System.out.println("Digite o quarto número!");
+        d = sc.nextInt();
+
+        value = a * b - c * d;
+
+        System.out.printf("A diferença entre o produto de %d, %d e %d, %d é de %d", a, b, c, d, value);
+    }
 
 }
